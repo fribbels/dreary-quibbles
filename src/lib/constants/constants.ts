5269db0e94c886fd1332301717e97a054e4d004b
fribbels <>
2025-01-08 04:46:27 +0000

    Update from https://github.com/fribbels/hsr-optimizer/commit/e37f24adfe3e67d376a350a819265022fa2ce2d5

--- src/lib/constants/constants.ts
+++ src/lib/constants/constants.ts
@@ -4,6 +4,7 @@ import gameData from 'data/game_data.json'
 // Semver defined optimizer version
 export const CURRENT_OPTIMIZER_VERSION = 'v3.0.5'
 // Represents the beta data content version, used for display but not for update notifications
+
 export const CURRENT_DATA_VERSION = '3.0v5'
 
 export const Stats = {
